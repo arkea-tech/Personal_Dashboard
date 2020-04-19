b1eb3b21b679d64f01d4f353cee9e6e676cfbe4a
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-04-19 14:15:27 +0200

    [ADD] POST Route Basic Ready

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,13 +10,22 @@ app
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 })
-.use('/api/stuff', (req, res, next) => {
+.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Thing created successfully !'
+    });
+});
+
+app.use('/api/stuff', (req, res, next) => {
     const stuff = [
         {
             _id: 'oeihfzeoi',
             title: 'My first thing',
             description: 'All of the info about my first thing',
-            imageUrl: '',
+            imageUrl: 'https://cdn.mos.cms.futurecdn.net/7UKru4akuGz2QcUPp6smqX.jpg',
             price: 4900,
             userId: 'qsomihvqios',
         },
@@ -23,7 +33,7 @@ app
             _id: 'oeihfzeomoihi',
             title: 'My second thing',
             description: 'All of the info about my second thing',
-            imageUrl: '',
+            imageUrl: 'https://cdn.mos.cms.futurecdn.net/dzDWYxpsXBvVtNBd8Cuao8.jpg',
             price: 2900,
             userId: 'qsomihvqios',
         }
