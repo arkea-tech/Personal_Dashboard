2cb1b643f1825b5d688c6820daff48137a7a0a77
WoshiWoshu <gabriel.pironneau@epitech.eu>
2020-04-24 20:33:02 +0200

    [ADD] User Model + Signup route implemented

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
